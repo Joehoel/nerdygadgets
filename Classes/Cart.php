2bd7fe16ad114ebf24e2ebbe31f869950098475c
Hoeishetermee1 <tim_bentum@live.nl>
2020-11-10 09:42:13 +0100

    Updates

--- Classes/Cart.php
+++ Classes/Cart.php
@@ -1,27 +1,46 @@
 <?php
 
-namespace NerdyGadgests\Classes{
+namespace NerdyGadgests\Classes {
     class Cart
     {
-        public function GetCartArray(){
-            session_start();
+        public function GetCartArray()
+        {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
             return $_SESSION['Cart'];
         }
 
-        public function AddItemToCart($artNr, $aantal){
-            session_start();
-            //kijken of al in array is... dan aantal toevoegen aan dat getal
-            //anders niewe line aan $_SESSION['Cart'] geven 
+        public function AddItemToCart($artNr, $aantal)
+        {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $cart = isset($_SESSION['Cart'])? $_SESSION['Cart'] : null;
+            if (isset($cart[$artNr])){
+                $cart[$artNr] += $aantal;
+            }
+            else{
+                $cart[$artNr] = $aantal;
+            }
+            $_SESSION['Cart'] = $cart;
         }
 
-        public function RemoveFromCart($artNr){
-            session_start();
+        public function RemoveFromCart($artNr)
+        {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
+            $cart = isset($_SESSION['Cart'])? $_SESSION['Cart'] : null;
+            unset($cart[$artNr]);
         }
 
-        public function UpdateCart(){
-            session_start();
+        public function UpdateCart()
+        {
+            if (session_status() == PHP_SESSION_NONE) {
+                session_start();
+            }
             //kijken op getallen te kunnen update in de cart en dit doen
         }
     }
-    
 }
--- cart.php
+++ cart.php
@@ -1,4 +1,18 @@
 <?php
+
 include "header.php";
+
+include 'Classes/Cart.php';
+
+use NerdyGadgests\Classes\Cart;
+
+$CartHandeler = new Cart();
+
+
+print_r($CartHandeler->GetCartArray());
+
+
+
+
 
 include "footer.php";
