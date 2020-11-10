1a94a3911633a93b2245aa7a7568a67c2e8e4d8d
Hoeishetermee1 <tim_bentum@live.nl>
2020-11-10 10:43:02 +0100

    Updates

--- Classes/Cart.php
+++ Classes/Cart.php
@@ -3,6 +3,12 @@
 namespace NerdyGadgests\Classes {
     class Cart
     {
+        /**
+         * Deze functie returnt alle data uit de cart
+         *
+         * @return Cart
+         * @author Tim Bentum <support@adjust-it.nl>
+         */
         public function GetCartArray()
         {
             if (session_status() == PHP_SESSION_NONE) {
@@ -11,6 +17,14 @@ namespace NerdyGadgests\Classes {
             return $_SESSION['Cart'];
         }
 
+        /**
+         * Toevoegen item aan cart
+         *
+         * @param int $artNr
+         * @param int $aantal
+         * @return void
+         * @author Tim Bentum <support@adjust-it.nl>
+         */
         public function AddItemToCart($artNr, $aantal)
         {
             if (session_status() == PHP_SESSION_NONE) {
@@ -26,6 +40,13 @@ namespace NerdyGadgests\Classes {
             $_SESSION['Cart'] = $cart;
         }
 
+        /**
+         * Remove artNR data uit de cart
+         *
+         * @param int $artNr
+         * @return void
+         * @author Tim Bentum <support@adjust-it.nl>
+         */
         public function RemoveFromCart($artNr)
         {
             if (session_status() == PHP_SESSION_NONE) {
@@ -35,6 +56,12 @@ namespace NerdyGadgests\Classes {
             unset($cart[$artNr]);
         }
 
+        /**
+         * Updaten van de cart
+         *
+         * @return void
+         * @author Tim Bentum <support@adjust-it.nl>
+         */
         public function UpdateCart()
         {
             if (session_status() == PHP_SESSION_NONE) {
--- cart.php
+++ cart.php
@@ -1,18 +1,54 @@
 <?php
 
 include "header.php";
-
 include 'Classes/Cart.php';
+include 'Classes/Database.php';
 
 use NerdyGadgests\Classes\Cart;
+use NerdyGadgests\Classes\DataBase;
 
 $CartHandeler = new Cart();
+$database = new DataBase();
+
+$cart = $CartHandeler->GetCartArray();
+
+$database->MakeConnection();
 
 
-print_r($CartHandeler->GetCartArray());
+foreach ($cart as $key => $value) {
+    $productData = $database->GetProductData($key);
+?>
+
+    <a class="ListItem" href='view.php?id=<?php print $productData['StockItemID']; ?>'>
+        <div id="ProductFrame">
+            <?php
+            if (isset($productData['ImagePath'])) { ?>
+                <div class="ImgFrame" style="background-image: url('<?php print "Public/StockItemIMG/" . $productData['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
+            <?php } else if (isset($productData['BackupImagePath'])) { ?>
+                <div class="ImgFrame" style="background-image: url('<?php print "Public/StockGroupIMG/" . $productData['BackupImagePath'] ?>'); background-size: cover;"></div>
+            <?php }
+            ?>
+
+            <div id="StockItemFrameRight">
+                <div class="CenterPriceLeftChild">
+                    <h1 class="StockItemPriceText"><?php print sprintf("€ %0.2f", $productData["SellPrice"]); ?></h1>
+                    <h6>Inclusief BTW </h6>
+                </div>
+            </div>
+            <h1 class="StockItemID">Artikelnummer: <?php print $productData["StockItemID"]; ?></h1>
+            <p class="StockItemName"><?php print $productData["StockItemName"]; ?></p>
+            <p class="StockItemComments"><?php print $productData["MarketingComments"]; ?></p>
+            <h4 class="ItemQuantity">Aantal: <?php print $value?></h4>
+        </div>
+    </a>
+
+<?php
+}
+
+$database->CloseConnection();
+?>
 
 
 
-
-
+<?php
 include "footer.php";
