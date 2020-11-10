1234f4e44f26c37b10371c49c3706b776b875fca
Tim <71019504+Vlixz@users.noreply.github.com>
2020-11-10 13:38:42 +0100

    Payment page

---
+++ payment.php
@@ -0,0 +1,90 @@
+<?php
+include __DIR__ . "/header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Public\SCSS\dist\payment.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content">
+        <div class="information">
+            <h1>Customer information</h1>
+            <form class="form-1">
+                <input type="text" placeholder="Email" name="email">
+                <div>
+                    <input type="checkbox" value="1" name="informed">
+                    <p>Keep me up to date on news and exlusive offers.</p>
+                </div>
+            </form>
+            <h1>shipping address</h1>
+            <form class="form-2">
+                <input type="text" placeholder="First name" name="f-name">
+                <input type="text" placeholder="Last name" name="l-name">
+                <input type="text" placeholder="Company (optional)" name="c-name">
+                <input type="text" placeholder="Address" name="address">
+                <input type="text" placeholder="Apt, suite, etc (optional)" name="apt">
+                <input type="text" placeholder="City" name="city">
+                <input type="text" placeholder="Country" name="country">
+                <input type="text" placeholder="Postal code" name="p-c">
+                <input type="text" placeholder="Phone" name="tel">
+            </form>
+        </div>
+
+        <div class="payment">
+            <table>
+                <tr>
+                    <td>Subtotaal</td>
+                    <td>€95,95</td>
+                </tr>
+                <tr>
+                    <td>Subtotaal (excl. BTW)</td>
+                    <td>€79,30</td>
+                </tr>
+                <tr>
+                    <td>BTW</td>
+                    <td>€16,65</td>
+                </tr>
+                <tr>
+                    <td>Verzendkosten</td>
+                    <td>Gratis</td>
+                </tr>
+                <tr>
+                    <td>Totaal</td>
+                    <td>€95,95</td>
+                </tr>
+            </table>
+            <div class="pay-container">
+                <form>
+                    <div class="inline-radio">
+                        <input type="radio" name="method" value="Credit">
+                        <p>Credit card</p>
+                    </div>
+                    <div class="credit-card">
+                        <input type="text" placeholder="Card number" name="card-number">
+                        <input type="text" placeholder="Name on card" name="card-name">
+                        <input class="start-column" type="text" placeholder="Expiration date (MM / YY)" name="card-number">
+                        <input class="end-column" type="text" placeholder="Security code" name="card-name">
+                    </div>
+                    <div class="inline-radio">
+                        <input type="radio" name="method"  value="PayPal">
+                        <p>PayPal</p>
+                    </div>
+                    <div class="inline-radio">
+                        <input type="radio" name="method"  value="IDEAL">
+                        <p>IDEAL</p>
+                    </div>
+                    <div class="IDEAL">
+                        <input type="text" placeholder="Bank name" name="method">
+                    </div>
+                </form>
+               
+            </div>
+            <button>Continue to shipping</button>
+        </div>
+    </div>
+</body>
+</html>
