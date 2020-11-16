dbe7cc742cfff0fac1e1294ca6d8375a0434cf19
Arnold Hamstra <youtube@minecraftercity.com>
2020-11-16 15:44:42 +0100

    link on homepage fixed

--- views/home.php
+++ views/home.php
@@ -3,12 +3,11 @@
 <div class="IndexStyle">
     <div class="col-11">
         <div class="TextPrice">
-            <a href="view.php?id=93">
+            <a href="/product/93">
                 <div class="TextMain">
                     "The Gu" red shirt XML tag t-shirt (Black) M
                 </div>
                 <ul id="ul-class-price">
-
                     <li class="HomePagePrice">€30.95</li>
                 </ul>
             </a>
