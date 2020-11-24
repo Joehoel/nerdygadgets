d10b550fa0807e7c765a3d742388d188e6cfc322
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-11-24 09:53:47 +0100

    Added format

--- app/migrations/reviews.sql
+++ app/migrations/reviews.sql
@@ -1,15 +1,17 @@
-CREATE TABLE Users (
+CREATE TABLE Users IF NOT EXISTS(
 	UserID int NOT NULL,
+    Username varchar(45) NOT NULL,
     Email varchar(45) NOT NULL,
     Password varchar(45) NOT NULL, # Hashed
-    PhoneNumber int,
-    StreetAdress varchar(45) NOT NULL,
+    PhoneNumber int NOT NULL,
+    Adress varchar(45) NOT NULL, # Format -> "<street>, <huisnummer>"
+    City varchar(45) NOT NULL,
     PostalCode varchar(20) NOT NULL,
     Company varchar(45),
     PRIMARY KEY (UserID)
 );
 
-CREATE TABLE Reviews (
+CREATE TABLE Reviews IF NOT EXISTS (
 	ReviewID int NOT NULL,
     ProductID int NOT NULL,
     UserID int NOT NULL,
