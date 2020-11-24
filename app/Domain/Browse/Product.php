908a61c27a252e7457c42fc6f19d7079d9a698ef
Tim <71019504+Vlixz@users.noreply.github.com>
2020-11-24 10:18:04 +0100

    Revert "Merge pull request #18 from Joehoel/US2"
    This reverts commit 28a395d2a7279588e30fdfedd567125aa701b806, reversing
    changes made to 5aaaee32c71b21829443da0ccd6b26fec36f343f.

--- app/Domain/Browse/Product.php
+++ app/Domain/Browse/Product.php
@@ -4,62 +4,7 @@ namespace App\Domain\Browse;
 
 class Product
 {
-    public function getProducts()
-    {
-        // set all the variables
-        $CategoryID = $_GET['category_id'] ?? "";
-        $SearchString = $_GET['search_string'] ?? "";
-        $searchValues = explode(" ", $SearchString);
 
-        if (isset($_GET["products_on_page"])) {
-            $ProductsOnPage = $_GET["products_on_page"];
-        } elseif (isset($_SESSION["products_on_page"])) {
-            $ProductsOnPage = $_SESSION["products_on_page"];
-        } else {
-            $ProductsOnPage = 50;
-            $_SESSION["products_on_page"] = 50;
-            $_GET["products_on_page"] = 50;
-        }
-
-        $PageNumber = $_GET["page_number"] ?? 0;
-        $Offset = $PageNumber * $ProductsOnPage;
-        $queryBuildResult = "";
-
-
-        if ($SearchString !== "") {
-            if (strpos("\\", $SearchString) === FALSE) {
-                for ($i = 0; $i < count($searchValues); $i++) {
-                    if ($i != 0) {
-                        $queryBuildResult .= "AND ";
-                    }
-                    $queryBuildResult .= "SI.SearchDetails LIKE '%$searchValues[$i]%' ";
-                }
-                if ($queryBuildResult != "") {
-                    $queryBuildResult .= " OR ";
-                }
-                if ($SearchString != "" || $SearchString != null) {
-                    $queryBuildResult .= "SI.StockItemID ='$SearchString'";
-                }
-            } else {
-                $SearchString = "";
-            }
-        }
-
-        $ReturnableResult = $this->fetchProducts($CategoryID, $queryBuildResult);
-
-        foreach ($ReturnableResult as $key => $StockName) {
-            $ReturnableResult[$key]["StockItemName"] = str_replace('"', "", $StockName["StockItemName"]);
-        }
-
-        $SortedArray = $this->SortProducts($ReturnableResult);
-
-        $ReturnThisArray = [];
-        $min = min((count($SortedArray) - $Offset), $ProductsOnPage);
-        for ($i = 0; $i < $min; $i++) {
-            array_push($ReturnThisArray, $SortedArray[$i + $Offset]);
-        }
-        return  $ReturnThisArray;
-    }
     public function SortProducts($ReturnableResult)
     {
         if (isset($_GET['sort'])) {
@@ -117,12 +62,43 @@ class Product
         return $ReturnableResult;
     }
 
-    public function fetchProducts($CategoryID, $queryBuildResult)
+    public function getProducts()
     {
-        // set the connection to the database
         $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets", 3306);
         mysqli_set_charset($Connection, 'latin1');
 
+        $CategoryID = $_GET['category_id'] ?? "";
+        $SearchString = $_GET['search_string'] ?? "";
+        $searchValues = explode(" ", $SearchString);
+        if (isset($_GET["products_on_page"])) {
+            $ProductsOnPage = $_GET["products_on_page"];
+        } else {
+            $ProductsOnPage = 50;
+            $_GET["products_on_page"] = $ProductsOnPage;
+        }
+
+        $ProductsOnPage = $_GET["products_on_page"];
+        if (isset($_GET['page_number'])) {
+            $PageNumber = $_GET['page_number'];
+        } else {
+            $PageNumber = 0;
+        }
+        $Offset = $PageNumber * $ProductsOnPage;
+        $queryBuildResult = "";
+        if ($SearchString != "") {
+            for ($i = 0; $i < count($searchValues); $i++) {
+                if ($i != 0) {
+                    $queryBuildResult .= "AND ";
+                }
+                $queryBuildResult .= "SI.SearchDetails LIKE '%$searchValues[$i]%' ";
+            }
+            if ($queryBuildResult != "") {
+                $queryBuildResult .= " OR ";
+            }
+            if ($SearchString != "" || $SearchString != null) {
+                $queryBuildResult .= "SI.StockItemID ='$SearchString'";
+            }
+        }
 
         if ($CategoryID == "") {
             if ($queryBuildResult != "") {
@@ -130,19 +106,22 @@ class Product
             }
 
             $Query = "
-            SELECT stockitemstockgroups.StockGroupID AS CategoryID, SI.StockItemID, SI.StockItemName, SI.MarketingComments, QuantityOnHand,
-            ROUND(TaxRate * RecommendedRetailPrice / 100 + RecommendedRetailPrice,2) as SellPrice,
-            (SELECT ImagePath
-            FROM stockitemimages
-            WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
-            (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
-            FROM stockitems SI
-            JOIN stockitemholdings SIH USING(stockitemid)
-            JOIN stockitemstockgroups USING(StockItemID)
-            GROUP BY StockItemID";
+                        SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, ROUND(TaxRate * RecommendedRetailPrice / 100 + RecommendedRetailPrice,2) as SellPrice,
+                        (CASE WHEN (SIH.QuantityOnHand) >= ? THEN 'Ruime voorraad beschikbaar.' ELSE CONCAT('Voorraad: ',QuantityOnHand) END) AS QuantityOnHand,
+                        (SELECT ImagePath
+                        FROM stockitemimages
+                        WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+                        (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+                        FROM stockitems SI
+                        JOIN stockitemholdings SIH USING(stockitemid)
+                        " . $queryBuildResult . "
+                        GROUP BY StockItemID
+                        LIMIT ?  OFFSET ?";
+            // ORDER BY " . $Sort . "
+
 
             $Statement = mysqli_prepare($Connection, $Query);
-            // mysqli_stmt_bind_param($Statement);
+            mysqli_stmt_bind_param($Statement, "iii", $ShowStockLevel, $ProductsOnPage, $Offset);
             mysqli_stmt_execute($Statement);
             $ReturnableResult = mysqli_stmt_get_result($Statement);
             $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
@@ -162,30 +141,25 @@ class Product
             }
 
             $Query = "
-            SELECT stockitemstockgroups.StockGroupID AS CategoryID, SI.StockItemID, SI.StockItemName, SI.MarketingComments, QuantityOnHand,
-            ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) AS SellPrice,
-            (SELECT ImagePath 
-	        FROM stockitemimages 
-            WHERE StockItemID = SI.StockItemID LIMIT 1) AS ImagePath,
-            (SELECT ImagePath 
-	        FROM stockgroups 
-            JOIN stockitemstockgroups 
-            USING(StockGroupID) 
-            WHERE StockItemID = SI.StockItemID LIMIT 1) AS BackupImagePath
-            FROM stockitems AS SI
-            JOIN stockitemholdings AS SIH USING(stockitemid)
-            JOIN stockitemstockgroups USING(StockItemID)
-            JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
-            WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
-            GROUP BY StockItemID;";
-
+                        SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, QuantityOnHand as voorraad,
+                        ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) as SellPrice,
+                        (CASE WHEN (SIH.QuantityOnHand) >= ? THEN 'Ruime voorraad beschikbaar.' ELSE CONCAT('Voorraad: ',QuantityOnHand) END) AS QuantityOnHand,
+                        (SELECT ImagePath FROM stockitemimages WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+                        (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+                        FROM stockitems SI
+                        JOIN stockitemholdings SIH USING(stockitemid)
+                        JOIN stockitemstockgroups USING(StockItemID)
+                        JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
+                        WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
+                        GROUP BY StockItemID
+                        LIMIT ? OFFSET ?";
+            //  ORDER BY " . $Sort . "
 
             $Statement = mysqli_prepare($Connection, $Query);
-            mysqli_stmt_bind_param($Statement, "i", $CategoryID);
+            mysqli_stmt_bind_param($Statement, "iiii", $ShowStockLevel, $CategoryID, $ProductsOnPage, $Offset);
             mysqli_stmt_execute($Statement);
             $ReturnableResult = mysqli_stmt_get_result($Statement);
             $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
-
 
             $Query = "
                         SELECT count(*)
@@ -197,6 +171,10 @@ class Product
             $Result = mysqli_stmt_get_result($Statement);
             $Result = mysqli_fetch_all($Result, MYSQLI_ASSOC);
             $_SESSION["results"] = $Result[0]["count(*)"];
+        }
+
+        foreach ($ReturnableResult as $key => $StockName) {
+            $ReturnableResult[$key]["StockItemName"] = str_replace('"', "", $StockName["StockItemName"]);
         }
         return $ReturnableResult;
     }
--- views/cart.php
+++ views/cart.php
@@ -4,68 +4,82 @@ include __DIR__ . "/connect.php";
 include __DIR__ . "/header.php";
 
 if (isset($_GET['update']) && $_GET['update'] === "true") {
-    echo '<script> popup("De winkelwagen is geüpdate") </script>';
+    echo '<div class="pop-up">De winkelwagen is geüpdate</div>';
 }
 
+$prices = $cartClass->GetTotalCartPrice()
+
 ?>
-<div class="pop-up" id="pop-up"></div>
-<div class="list">
-    <?php
-    if (count($cart) == 0) {
+<div class="cart">
+    <?php if (count($cart) == 0) {
         echo "<div class='empty'>
-            <h4>De winkelwagen is leeg, klik <a class='empty-cart-link' href='" . base_url . "categories'>hier</a> om iets aan de winkelwagen toetevoegen</h4>
-        </div>";
-    }
-    foreach ($cart as $key => $value) {
-        $productData = $cartClass->GetProductData($key);
-    ?>
-
-        <div class="list-item">
-
+    <h4>De winkelwagen is leeg, klik <a class='empty-cart-link' href='" . base_url . "categories'>hier</a> om iets aan de winkelwagen toetevoegen</h4>
+    </div>";
+    } else { ?>
+        <div class="list">
             <?php
-            if (isset($productData['ImagePath'])) { ?>
-                <div class="ImgFrame" style="background-image: url('<?php print "Public/StockItemIMG/" . $productData['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
-            <?php } else if (isset($productData['BackupImagePath'])) { ?>
-                <div class="ImgFrame" style="background-image: url('<?php print "Public/StockGroupIMG/" . $productData['BackupImagePath'] ?>'); background-size: cover;"></div>
-            <?php }
+            foreach ($cart as $key => $value) {
+                $productData = $cartClass->GetProductData($key);
             ?>
-            <div class="item-info">
-                <div>
-                    <h1>Artikelnummer: <?php print $productData["StockItemID"]; ?></h1>
-                    <a href='product/<?php print $productData['StockItemID']; ?>'>
-                        <p><?php print $productData["StockItemName"]; ?></p>
-                        <p><?php print $productData["MarketingComments"]; ?></p>
-                    </a>
-                    <form action="<?php echo base_url; ?>update-cart/<?php echo $productData["StockItemID"]; ?>" method="POST" class="update-count">
-                        <label for="aantal">Aantal: </label>
-                        <input type="number" name="aantal" value="<?php print $value ?>" min="0" max="<?php echo $productData['voorraad'] ?>">
-                        <input type="submit" value="Update">
-                    </form>
-                </div>
-                <div>
-                    <form class="delete-form" action="<?php echo base_url ?>delete-from-cart/<?php echo $productData["StockItemID"] ?>">
-                        <button type="submit" class="delete-button">
 
-                            <svg style="width: 25px; height: 25px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                            </svg>
-                        </button>
-                    </form>
-                    <div class="price">
-                        <!-- Weet niet zeker of de prijs klopt -->
-                        <h2><?php print sprintf("€%0.2f", $productData["SellPrice"] * $value); ?></h2>
-                        <h6>Inclusief BTW </h6>
+                <div class="list-item">
+
+                    <?php
+                    if (isset($productData['ImagePath'])) { ?>
+                        <div class="ImgFrame" style="background-image: url('<?php print "Public/StockItemIMG/" . $productData['ImagePath']; ?>'); background-size: 230px; background-repeat: no-repeat; background-position: center;"></div>
+                    <?php } else if (isset($productData['BackupImagePath'])) { ?>
+                        <div class="ImgFrame" style="background-image: url('<?php print "Public/StockGroupIMG/" . $productData['BackupImagePath'] ?>'); background-size: cover;"></div>
+                    <?php }
+                    ?>
+                    <div class="item-info">
+                        <div>
+                            <h1>Artikelnummer: <?php print $productData["StockItemID"]; ?></h1>
+                            <a href='product/<?php print $productData['StockItemID']; ?>'>
+                                <p><?php print $productData["StockItemName"]; ?></p>
+                                <p><?php print $productData["MarketingComments"]; ?></p>
+                            </a>
+                            <form action="<?php echo base_url; ?>update-cart/<?php echo $productData["StockItemID"]; ?>" method="POST" class="update-count">
+                                <label for="aantal">Aantal: </label>
+                                <input type="number" name="aantal" value="<?php print $value ?>" min="0" max="<?php echo $productData['voorraad'] ?>">
+                                <input type="submit" value="Update">
+                            </form>
+                        </div>
+                        <div>
+                            <form class="delete-form" action="<?php echo base_url ?>delete-from-cart/<?php echo $productData["StockItemID"] ?>">
+                                <button type="submit" class="delete-button">
+                                    <img src="<?php echo base_url ?>Public/Img/delete.svg" alt="delete">
+                                </button>
+                            </form>
+                            <div class="price">
+                                <!-- Weet niet zeker of de prijs klopt -->
+                                <h2><?php print sprintf("€%0.2f", $productData["SellPrice"] * $value); ?></h2>
+                                <h6>Inclusief BTW </h6>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    <?php } ?>
+            <?php } ?>
+        <?php } ?>
 
-    <?php if (count($cart) > 0) { ?>
-        <div class="next">
-            <a class="to-payments" href="<?php echo base_url ?>checkout">Verder naar checkout</a>
         </div>
-    <?php } ?>
+        <?php if (count($cart) > 0) { ?>
+            <div class="total">
+                <div class="item">
+                    <h3>Totaal artikelen:</h3><span><?php echo $prices['articleTotal']; ?></span>
+                </div>
+                <div class="item">
+                    <h5>Korting:</h5><span><?php echo $prices['discount']; ?></span>
+                </div>
+                <div class="item">
+                    <h5>Verzendkosten:</h5><span><?php echo $prices['shipping']; ?></span>
+                </div>
+                <hr>
+                <div class="item">
+                    <h3>Totaal:</h3><span><?php echo $prices['total']; ?></span>
+                </div>
+                <a class="to-checkout" href="<?php echo base_url ?>checkout">Verder naar checkout</a>
+            </div>
+        <?php } ?>
 </div>
 
 <?php
