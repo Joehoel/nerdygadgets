46228331f0e533e40b89cc540a493558569b7732
Hoeishetermee1 <tim_bentum@live.nl>
2020-11-24 11:27:25 +0100

    Revert "Error verholpen"
    This reverts commit 5474821b530cfb4e5d0a3b7024b731d86b69a987.

--- app/Domain/Browse/Product.php
+++ app/Domain/Browse/Product.php
@@ -4,7 +4,62 @@ namespace App\Domain\Browse;
 
 class Product
 {
+    public function getProducts()
+    {
+        // set all the variables
+        $CategoryID = $_GET['category_id'] ?? "";
+        $SearchString = $_GET['search_string'] ?? "";
+        $searchValues = explode(" ", $SearchString);
 
+        if (isset($_GET["products_on_page"])) {
+            $ProductsOnPage = $_GET["products_on_page"];
+        } elseif (isset($_SESSION["products_on_page"])) {
+            $ProductsOnPage = $_SESSION["products_on_page"];
+        } else {
+            $ProductsOnPage = 50;
+            $_SESSION["products_on_page"] = 50;
+            $_GET["products_on_page"] = 50;
+        }
+
+        $PageNumber = $_GET["page_number"] ?? 0;
+        $Offset = $PageNumber * $ProductsOnPage;
+        $queryBuildResult = "";
+
+
+        if ($SearchString !== "") {
+            if (strpos("\\", $SearchString) === FALSE) {
+                for ($i = 0; $i < count($searchValues); $i++) {
+                    if ($i != 0) {
+                        $queryBuildResult .= "AND ";
+                    }
+                    $queryBuildResult .= "SI.SearchDetails LIKE '%$searchValues[$i]%' ";
+                }
+                if ($queryBuildResult != "") {
+                    $queryBuildResult .= " OR ";
+                }
+                if ($SearchString != "" || $SearchString != null) {
+                    $queryBuildResult .= "SI.StockItemID ='$SearchString'";
+                }
+            } else {
+                $SearchString = "";
+            }
+        }
+
+        $ReturnableResult = $this->fetchProducts($CategoryID, $queryBuildResult);
+
+        foreach ($ReturnableResult as $key => $StockName) {
+            $ReturnableResult[$key]["StockItemName"] = str_replace('"', "", $StockName["StockItemName"]);
+        }
+
+        $SortedArray = $this->SortProducts($ReturnableResult);
+
+        $ReturnThisArray = [];
+        $min = min((count($SortedArray) - $Offset), $ProductsOnPage);
+        for ($i = 0; $i < $min; $i++) {
+            array_push($ReturnThisArray, $SortedArray[$i + $Offset]);
+        }
+        return  $ReturnThisArray;
+    }
     public function SortProducts($ReturnableResult)
     {
         if (isset($_GET['sort'])) {
@@ -62,84 +117,32 @@ class Product
         return $ReturnableResult;
     }
 
-<<<<<<< HEAD
-    public function getProducts()
+    public function fetchProducts($CategoryID, $queryBuildResult)
     {
+        // set the connection to the database
         $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets", 3306);
         mysqli_set_charset($Connection, 'latin1');
 
-        // set all the variables
 
-        $CategoryID = $_GET['category_id'] ?? "";
-        $SearchString = $_GET['search_string'] ?? "";
-        $searchValues = explode(" ", $SearchString);
-        $ProductsOnPage = $_GET["products_on_page"] ?? 50;
-        $PageNumber = $_GET["page_number"] ?? 0;
-        $Offset = $PageNumber * $ProductsOnPage;
-        $queryBuildResult = "";
-
-        // set all the variables
-        $CategoryID = $_GET['category_id'] ?? "";
-        $SearchString = $_GET['search_string'] ?? "";
-        $searchValues = explode(" ", $SearchString);
-        $ProductsOnPage = $_GET["products_on_page"] ?? 50;
-        $PageNumber = $_GET["page_number"] ?? 0;
-        $Offset = $PageNumber * $ProductsOnPage;
-        $queryBuildResult = "";
-        if ($SearchString != "") {
-            for ($i = 0; $i < count($searchValues); $i++) {
-                if ($i != 0) {
-                    $queryBuildResult .= "AND ";
-                }
-                $queryBuildResult .= "SI.SearchDetails LIKE '%$searchValues[$i]%' ";
-            }
-            if ($queryBuildResult != "") {
-                $queryBuildResult .= " OR ";
-            }
-            if ($SearchString != "" || $SearchString != null) {
-                $queryBuildResult .= "SI.StockItemID ='$SearchString'";
-            }
-        }
-
-        if ($SearchString !== "") {
-            if (strpos("\\", $SearchString) === FALSE) {
-                for ($i = 0; $i < count($searchValues); $i++) {
-                    if ($i != 0) {
-                        $queryBuildResult .= "AND ";
-                    }
-                    $queryBuildResult .= "SI.SearchDetails LIKE '%$searchValues[$i]%' ";
-                }
-                if ($queryBuildResult != "") {
-                    $queryBuildResult .= " OR ";
-                }
-                if ($SearchString != "" || $SearchString != null) {
-                    $queryBuildResult .= "SI.StockItemID ='$SearchString'";
-                }
-            } else {
-                $SearchString = "";
-            }
-        }
         if ($CategoryID == "") {
             if ($queryBuildResult != "") {
                 $queryBuildResult = "WHERE " . $queryBuildResult;
             }
 
             $Query = "
-                        SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, ROUND(TaxRate * RecommendedRetailPrice / 100 + RecommendedRetailPrice,2) as SellPrice,
-                        (CASE WHEN (SIH.QuantityOnHand) >= ? THEN 'Ruime voorraad beschikbaar.' ELSE CONCAT('Voorraad: ',QuantityOnHand) END) AS QuantityOnHand,
-                        (SELECT ImagePath
-                        FROM stockitemimages
-                        WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
-                        (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
-                        FROM stockitems SI
-                        JOIN stockitemholdings SIH USING(stockitemid)
-                        " . $queryBuildResult . "
-                        GROUP BY StockItemID";
+            SELECT stockitemstockgroups.StockGroupID AS CategoryID, SI.StockItemID, SI.StockItemName, SI.MarketingComments, QuantityOnHand,
+            ROUND(TaxRate * RecommendedRetailPrice / 100 + RecommendedRetailPrice,2) as SellPrice,
+            (SELECT ImagePath
+            FROM stockitemimages
+            WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
+            (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
+            FROM stockitems SI
+            JOIN stockitemholdings SIH USING(stockitemid)
+            JOIN stockitemstockgroups USING(StockItemID)
+            GROUP BY StockItemID";
 
             $Statement = mysqli_prepare($Connection, $Query);
-            mysqli_stmt_bind_param($Statement, "iii", $ShowStockLevel, $ProductsOnPage);
-
-
+            // mysqli_stmt_bind_param($Statement);
             mysqli_stmt_execute($Statement);
             $ReturnableResult = mysqli_stmt_get_result($Statement);
             $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
@@ -159,24 +162,30 @@ class Product
             }
 
             $Query = "
-                        SELECT SI.StockItemID, SI.StockItemName, SI.MarketingComments, QuantityOnHand as voorraad,
-                        ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) as SellPrice,
-                        (CASE WHEN (SIH.QuantityOnHand) >= ? THEN 'Ruime voorraad beschikbaar.' ELSE CONCAT('Voorraad: ',QuantityOnHand) END) AS QuantityOnHand,
-                        (SELECT ImagePath FROM stockitemimages WHERE StockItemID = SI.StockItemID LIMIT 1) as ImagePath,
-                        (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath
-                        FROM stockitems SI
-                        JOIN stockitemholdings SIH USING(stockitemid)
-                        JOIN stockitemstockgroups USING(StockItemID)
-                        JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
-                        WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
-                        GROUP BY StockItemID";
+            SELECT stockitemstockgroups.StockGroupID AS CategoryID, SI.StockItemID, SI.StockItemName, SI.MarketingComments, QuantityOnHand,
+            ROUND(SI.TaxRate * SI.RecommendedRetailPrice / 100 + SI.RecommendedRetailPrice,2) AS SellPrice,
+            (SELECT ImagePath 
+	        FROM stockitemimages 
+            WHERE StockItemID = SI.StockItemID LIMIT 1) AS ImagePath,
+            (SELECT ImagePath 
+	        FROM stockgroups 
+            JOIN stockitemstockgroups 
+            USING(StockGroupID) 
+            WHERE StockItemID = SI.StockItemID LIMIT 1) AS BackupImagePath
+            FROM stockitems AS SI
+            JOIN stockitemholdings AS SIH USING(stockitemid)
+            JOIN stockitemstockgroups USING(StockItemID)
+            JOIN stockgroups ON stockitemstockgroups.StockGroupID = stockgroups.StockGroupID
+            WHERE " . $queryBuildResult . " ? IN (SELECT StockGroupID from stockitemstockgroups WHERE StockItemID = SI.StockItemID)
+            GROUP BY StockItemID;";
+
 
             $Statement = mysqli_prepare($Connection, $Query);
-            mysqli_stmt_bind_param($Statement, "ii", $ShowStockLevel, $CategoryID);
-
+            mysqli_stmt_bind_param($Statement, "i", $CategoryID);
             mysqli_stmt_execute($Statement);
             $ReturnableResult = mysqli_stmt_get_result($Statement);
             $ReturnableResult = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC);
+
 
             $Query = "
                         SELECT count(*)
@@ -189,18 +198,6 @@ class Product
             $Result = mysqli_fetch_all($Result, MYSQLI_ASSOC);
             $_SESSION["results"] = $Result[0]["count(*)"];
         }
-
-        foreach ($ReturnableResult as $key => $StockName) {
-            $ReturnableResult[$key]["StockItemName"] = str_replace('"', "", $StockName["StockItemName"]);
-        }
-
-        $SortedArray = $this->SortProducts($ReturnableResult);
-
-        $ReturnThisArray = [];
-        $min = min((count($SortedArray) - $Offset), $ProductsOnPage);
-        for ($i = 0; $i < $min; $i++) {
-            array_push($ReturnThisArray, $SortedArray[$i + $Offset]);
-        }
-        return  $ReturnThisArray;
+        return $ReturnableResult;
     }
 }
