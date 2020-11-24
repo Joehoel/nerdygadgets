5205af6cef60db2fd8c5c81929a862fcac210c3e
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-11-24 09:59:25 +0100

    Updated migrations

--- app/migrations/users.sql
+++ app/migrations/users.sql
@@ -10,14 +10,3 @@ CREATE TABLE IF NOT EXISTS Users (
     Company varchar(45),
     PRIMARY KEY (UserID)
 );
-
-CREATE TABLE IF NOT EXISTS Reviews  (
-	ReviewID int NOT NULL,
-    ProductID int NOT NULL,
-    UserID int NOT NULL,
-    Rating int NOT NULL,
-    Text varchar(1000) NOT NULL,
-    PRIMARY KEY (ReviewID),
-    FOREIGN KEY (ProductID) REFERENCES StockItems(StockItemID),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
-);
