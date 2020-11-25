a83f4bba995b80ed94b993b412cc2efc6f76207b
Tim <71019504+Vlixz@users.noreply.github.com>
2020-11-25 11:57:13 +0100

    fixed merge

--- Public/JS/popup.js
+++ Public/JS/popup.js
@@ -1,14 +1,17 @@
-function popup(text) {
+function popup(text, isError) {
     window.addEventListener("load", () => {
         const pop_up = document.getElementById("pop-up");
         pop_up.innerText = text;
-        pop_up.classList = "pop-up pop-up-show";
-        setTimeout(() => {
-            pop_up.classList = "pop-up pop-up-hidden";
-            setTimeout(() => {
-                pop_up.classList = "pop-up";
-            }, 200);
-        }, 3000);
-    })
+        if (isError) {
 
+        } else {
+            pop_up.classList = "pop-up pop-up-show";
+            setTimeout(() => {
+                pop_up.classList = "pop-up pop-up-hidden";
+                setTimeout(() => {
+                    pop_up.classList = "pop-up";
+                }, 200);
+            }, 3000);
+        }
+    })
 }
--- app/Domain/Registreren/Registreren.php
+++ app/Domain/Registreren/Registreren.php
@@ -6,23 +6,52 @@ use App\Domain\Database\DatabaseInstance;
 use mysqli;
 use mysqli_stmt;
 
-Class Registreren {
+class Registreren
+{
+    public function checkGegevens()
+    {
+        $user = [
+            "voornaam" => $_POST["f-name"] ?? "",
+            "achternaam" => $_POST['l-name'] ?? "",
+            "adres" => $_POST['address'] ?? "",
+            "postcode" => $_POST['p-c'] ?? "",
+            "email" => $_POST['email'] ?? "",
+            "telefoonNummer" => $_POST['tel'],
+            "bedrijf" => $_POST['c-name'] ?? "",
+            "city" => $_POST['city'] ?? "",
+            "wachtwoord" => $_POST['password-1'] ?? ""
+        ];
+        foreach ($user as $key => $value) {
+            switch ($value) {
+                case '':
+                    return "missende " . $key;
+                    break;
+                case 'wachtwoord':
+                    if ($value != $_POST['password-2']) {
+                        return "Password do not match";
+                    }
+                    break;
+            }
+        }
+        $this->InsertGegevens($user);
+    }
 
-
-    public function InsertGegevens() {
+    public function InsertGegevens()
+    {
         $connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
         mysqli_set_charset($connection, 'latin1');
 
         $sql = "INSERT INTO users U (Email, FirstName, LastName, Password, PhoneNumber, Adress, City, PostalCode, Company)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
         $stm = mysqli_prepare($connection, $sql);
-        mysqli_stmt_bind_param($stm,"iiiiiiiii",$email,$voornaam,$achternaam,$wachtwoord,$telefoonNummer,$straat,$stad,$postcode,$bedrijf);
+        mysqli_stmt_bind_param($stm, "iiiiiiiii", $email, $voornaam, $achternaam, $wachtwoord, $telefoonNummer, $straat, $stad, $postcode, $bedrijf);
         mysqli_stmt_execute($stm);
         $result = mysqli_stmt_get_result($stm);
-        $result = mysqli_fetch_all($result, MYSQLI_ASSOC); 
+        $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
-    public function EmailCheck() {
+    public function EmailCheck()
+    {
         $connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
         mysqli_set_charset($connection, 'latin1');
 
@@ -31,7 +60,6 @@ Class Registreren {
                 WHERE Email IN ?";
 
         $stm = mysqli_prepare($connection, $sql);
-        mysqli_stmt_bind_param($stm,"i",$email);
+        mysqli_stmt_bind_param($stm, "i", $email);
     }
 }
-?>
