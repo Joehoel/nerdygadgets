51cb58c864b811050b7ca1be1a0a27ce628ee302
Tim <71019504+Vlixz@users.noreply.github.com>
2020-11-30 11:25:10 +0100

    Update users.sql

--- config/migrations/users.sql
+++ config/migrations/users.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS Users (
+CCREATE TABLE IF NOT EXISTS Users (
     UserID int NOT NULL AUTO_INCREMENT,
     Email varchar(45) NOT NULL,
     FirstName varchar(45) NOT NULL,
     LastName varchar(45) NOT NULL,
     Password char(60) NOT NULL,
     /* Hashed */
-    PhoneNumber int NOT NULL,
+    PhoneNumber varchar(45) NOT NULL,
     Adress varchar(45) NOT NULL,
     /* Format->"<street>, <huisnummer>" */
     City varchar(45) NOT NULL,
