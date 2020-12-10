44118052da9be131df02d2227b52524644cc3146
Hoeishetermee1 <tim_bentum@live.nl>
2020-12-10 18:02:47 +0100

    betere phone regex

--- app/Domain/Validation/Type/Phone.php
+++ app/Domain/Validation/Type/Phone.php
@@ -6,7 +6,7 @@ class Phone
 {
     public function rule($value)
     {
-        if(preg_match('/^[0-9\-\(\)\/\+\s]*$/', $value)) {
+        if(preg_match('/^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9])((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]$/', $value)) {
             return true;
         }
         return false;
