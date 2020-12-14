5d99916240dc0be2e1c6bc5f26b26bac18fe1246
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-12-14 14:15:36 +0100

    Fixed formatting

--- app/Controller/ReviewController.php
+++ app/Controller/ReviewController.php
@@ -14,8 +14,12 @@ class ReviewController
    */
   public function create($id)
   {
-    if (!isset($_SESSION['User']['UserID'])) return header("Location: " . base_url . "product/" . $id . "?error= " . _("Je moet ingelogd zijn om een review te maken"));
-    if (isset($_POST['review-text']) && strlen($_POST['review-text']) > 1000) return header("Location: " . base_url . "product/" . $id . "?error= " . _("Review mag maximaal 1000 tekens zijn"));
+    if (!isset($_SESSION['User']['UserID'])) {
+      return header("Location: " . base_url . "product/" . $id . "?error= " . _("Je moet ingelogd zijn om een review te maken"));
+    }
+    if (isset($_POST['review-text']) && strlen($_POST['review-text']) > 1000){
+      return header("Location: " . base_url . "product/" . $id . "?error= " . _("Review mag maximaal 1000 tekens zijn"));
+    }
     if (isset($_POST['rating']) && isset($_POST['review-text']) && !empty($_POST['review-text'])) {
       $rating = (int) $_POST['rating'];
       if ($rating < 1 || $rating > 5) {
