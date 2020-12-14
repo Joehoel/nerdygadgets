561b024586766c03185d753c631c3fc0e1729450
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-12-14 14:27:57 +0100

    Fixed bug

--- app/Controller/ReviewController.php
+++ app/Controller/ReviewController.php
@@ -17,9 +17,15 @@ class ReviewController
     if (!isset($_SESSION['User']['UserID'])) {
       return header("Location: " . base_url . "product/" . $id . "?error= " . _("Je moet ingelogd zijn om een review te maken"));
     }
-    if (isset($_POST['review-text']) && strlen($_POST['review-text']) > 1000){
+
+    if (isset($_POST['review-text']) && strlen($_POST['review-text']) > 1000) {
       return header("Location: " . base_url . "product/" . $id . "?error= " . _("Review mag maximaal 1000 tekens zijn"));
     }
+
+    if (isset($_POST['review-text']) || strlen($_POST['review-text']) < 1) {
+      return header("Location: " . base_url . "product/" . $id . "?error= " . _("Je moet alle velden invullen"));
+    }
+
     if (isset($_POST['rating']) && isset($_POST['review-text']) && !empty($_POST['review-text'])) {
       $rating = (int) $_POST['rating'];
       if ($rating < 1 || $rating > 5) {
