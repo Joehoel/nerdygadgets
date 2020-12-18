b2d2084ffd57e62bb738c6e4f11a9ab2b0ed84b2
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-12-18 14:57:06 +0100

    Add SQL

---
+++ config/migrations/order.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `weborders` (
+	OrderID INT(11) PRIMARY KEY AUTO_INCREMENT,
+    UserID INT(11),
+    PaymentMethod varchar(45) NOT NULL,
+    BankName varchar(45),
+    CartCVC varchar(45),
+    CartHolder varchar(45),
+    ExpDate varchar(45),
+
+    FOREIGN KEY (UserID) REFERENCES users(UserID)
+);
+
+CREATE TABLE `weborderlines` (
+	OrderlineID INT(11) AUTO_INCREMENT,
+	OrderID INT(11) NOT NULL,
+    ProductID INT(11) NOT NULL,
+    PRIMARY KEY (OrderlineID),
+    FOREIGN KEY (OrderID) REFERENCES weborders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES stockitems(StockItemID)
+);
